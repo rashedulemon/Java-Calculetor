64d6089bd17be1db6da96c12d93cabdb1152fc26
Rashedul Islam Emon <35653087+rashedulemon@users.noreply.github.com>
2018-07-01 12:58:49 +0600

    Create Formula.java

---
+++ src/Formula.java
@@ -0,0 +1,18 @@
+package add.calculetor;
+
+public class Formula {
+	
+	int add(int a, int b) {
+		int result = a + b;
+		return result;
+	}
+	
+	double add(double a, double b) {
+		double result = a + b;
+		return result;
+	}
+	
+	void add() {
+		System.out.println("Nothing to print!!");
+	}
+}
